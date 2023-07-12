7ba0c5e84aa58bf8b315ea07f628662ea462ab06
Chhan Sophearith <44798886+chhan-sophearith@users.noreply.github.com>
2023-07-12 14:20:16 +0700

    Add files via upload

---
+++ Tic-tac-toe/tic-tac-toe-swift/TicTacToe/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  TicTacToe
+//
+//  Created by chhan sophearith on 10/07/2023
+//  Copyright © 2023 chhans ophearith All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+}
+
---
+++ Tic-tac-toe/tic-tac-toe-swift/TicTacToe/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  TicTacToe
+//
+//  Created by chhan sophearith on 10/07/2023
+//  Copyright © 2023 chhans ophearith All rights reserved.
+//
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var ticTacToe = TicTacToe()
+    @IBOutlet var lblStatus: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateState()
+    }
+
+    @IBAction func buttonTapped(sender: UIButton) {
+        if sender.title(for: .normal) != "-" {
+            return
+        }
+        var btnTextValue: String
+        var btnBoardValue: Int
+        switch ticTacToe.checkStatus() {
+        case .xTern:
+            btnTextValue = "X"
+            btnBoardValue = 1
+        case .oTern:
+            btnTextValue = "O"
+            btnBoardValue = 2
+        default:
+            btnTextValue = "-"
+            btnBoardValue = 0
+        }
+        ticTacToe.board[(sender.tag-1)/3][(sender.tag-1)%3] = btnBoardValue
+        sender.setTitle(btnTextValue, for: .normal)
+        updateState()
+    }
+
+    @IBAction func btnResetTapped(sender: UIButton) {
+        ticTacToe.resetBoard()
+        for i in 1...9 {
+            let btn = self.view.viewWithTag(i) as! UIButton
+            btn.setTitle("-", for: .normal)
+        }
+        updateState()
+    }
+
+    func updateState() {
+        lblStatus.text = ticTacToe.checkStatus().rawValue
+    }
+
+}
+
